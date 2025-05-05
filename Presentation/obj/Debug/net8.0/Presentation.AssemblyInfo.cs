c672434f3bed1cb4749394388a7489d9a55bd147
davidMedhatM <david.medhat.m@gmail.com>
2025-05-05 10:38:37 +0300

    C43-G03-Api07

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e66b140a787125cd331d271d82232fc401dfa50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c91a3b00a467f53fab3ad8f5a370aa4f49f522")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
